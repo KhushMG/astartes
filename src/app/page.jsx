0b8b1545ed0d8c5b401fb93559557e40e31e1e11
KhushMG <khushh.meet@gmail.com>
2024-01-14 20:03:25 -0800

    Implemented Search functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,32 +2,44 @@
 import supabase from "../../utils/supabaseConfig";
 import { useEffect, useState } from "react";
 import AstarteList from "../components/AstarteList";
+import SearchBar from "@/components/SearchBar";
 
 
-export default function Home() {
+export default function Home({ searchParams }) {
   const [astartesList, setAstartesList] = useState([]);
+  const query = searchParams?.query || '';
 
   useEffect(() => {
     // Function to fetch data from Supabase
-    const fetchData = async () => {
-      const { data, error } = await supabase
+    const fetchData = async ( query ) => {
+      let queryBuilder = supabase
         .from("astartes")
         .select()
         .order("allegiance", { ascending: true });
-      if (error) { 
-        console.log("error fetching data")
+
+      if (query) {
+        queryBuilder = queryBuilder.ilike("name", `%${query}%`); 
       }
-      setAstartesList(data);
+
+      const { data, error } = await queryBuilder;
+
+      if (error) {
+        console.log("error fetching data");
+      } else {
+        setAstartesList(data);
+      }
     };
 
-    fetchData();
-  }, []);
+    fetchData(query);
+  }, [query]);
 
   return (
-    <main className="flex">
-      <div className="flex justify-center"> Astartes List </div>
-      <div>
-          <AstarteList astartesList={astartesList} />
+    <main>
+      <div className="mt-[2rem]">
+        <SearchBar query={query}/>
+      </div>
+      <div className="mt-[2rem] mx-[2rem]">
+        <AstarteList astartesList={astartesList} />
       </div>
     </main>
   );
