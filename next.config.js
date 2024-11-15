0f009ca81917e7bce25d0f43bf4a400e2667a342
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2024-11-15 14:40:34 -0800

    updating

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-require("dotenv").config();
+// @ts-check
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME,
-        port: "",
-      },
-    ],
+    domains: ["ualvseeqidsukwbmhhwk.supabase.co"],
   },
 };
 
